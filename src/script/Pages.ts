01a8d6f7e5ac732e1c5c5cdb566b73bc93d2d72a
vyytx <dobeender@gmail.com>
2023-11-12 14:02:49 +0800

    change to fontawesome icon

--- src/script/Pages.ts
+++ src/script/Pages.ts
@@ -1,38 +1,48 @@
 import type { ComponentType } from 'svelte'
-import { HomeSolid, ChartSolid, ChartBars3FromLeftSolid } from 'flowbite-svelte-icons'
+import { faHouse, faChartLine, faChartColumn, faChartPie, type IconDefinition } from '@fortawesome/free-solid-svg-icons/index'
 
+import Home from '../pages/Home.svelte'
 import HW1 from '../pages/HW1.svelte'
 import HW2 from '../pages/HW2.svelte'
 import HW3 from '../pages/HW3.svelte'
+import HW4 from '../pages/HW4.svelte'
+import RealData from '../pages/RealData.svelte'
 
 type Page = {
 	name: string,
-	icon: ComponentType,
+	icon: IconDefinition,
 	page?: ComponentType
 }
 
 export const Pages = [
 	{
-		name: 'Home',
-		icon: HomeSolid
+		name: '首頁',
+		icon: faHouse,
+		page: Home
 	},
 	{
-		name: 'HW1',
-		icon: ChartSolid,
+		name: '作業 1',
+		icon: faChartLine,
 		page: HW1
 	},
 	{
-		name: 'HW2',
-		icon: ChartSolid,
+		name: '作業 2',
+		icon: faChartLine,
 		page: HW2
 	},
 	{
-		name: 'HW3',
-		icon: ChartSolid,
+		name: '作業 3',
+		icon: faChartColumn,
 		page: HW3
 	},
 	{
-		name: 'HW4',
-		icon: ChartBars3FromLeftSolid
+		name: '作業 4',
+		icon: faChartPie,
+		page: HW4
+	},
+	{
+		name: '真實資料',
+		icon: faChartColumn,
+		page: RealData
 	}
 ] as const satisfies readonly Page[];
